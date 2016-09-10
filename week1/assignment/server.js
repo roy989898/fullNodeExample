213112d81e26dec51d18e1f9b2fe7bf752a566d7
roy989898 <roy989898@gmail.com>
2016-09-10 20:57:51 +0800

    finish the week 1 assignment

---
+++ week1/assignment/server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by User on 10/9/2016.
+ */
+var express = require('express');
+var morgan = require('morgan');
+var dishRouter = require('./dishRouter')();
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+
+
+app.use('/dishes', dishRouter);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function () {
+    console.log('Server running at http://' + hostname + ":" + port);
+});
